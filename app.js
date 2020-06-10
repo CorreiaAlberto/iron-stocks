60d04260752d4e0f7ffb16d27d5ddcf87e6ca9b8
Alberto Correia <alberto_cpv@hotmail.com>
2020-06-10 19:11:25 +0200

    pagina signup

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const bcrypt = require("bcryptjs")
 
 
 mongoose
-  .connect('mongodb://localhost/iron-stock', {useNewUrlParser: true})
+  .connect('mongodb://localhost/iron-stock', {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -27,17 +30,19 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -59,5 +64,8 @@ app.use('/', searchCompany)
 
 const showCompany = require('./routes/show-company');
 app.use('/', showCompany)
+//Ruta para singup
+const signUpRouter = require("./routes/auth.routes")
+app.use('/', signUpRouter)
 
 module.exports = app;
